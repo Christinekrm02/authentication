f3be819b2049bb1d6cc0d1bb31879b6cc38a06b8
Christine Maynard <christinekrm02@gmail.com>
2021-06-09 20:54:13 -0400

    Update User model to include password hasing functionality

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -18,5 +19,29 @@ const UserSchema = mongoose.Schema({
     default: Date.now(),
   },
 });
+
+//HASH PASSWORD
+/*Avoid using arrow function on object prototype because
+arrow binds the context lexically with the window object.
+thus returning undefined */
+UserSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next;
+  bcrypt.genSalt(10, function (e, salt) {
+    if (e) throw next(e);
+    bcrypt.hash(user.password, salt, function (e, hash) {
+      if (e) throw next(e);
+      user.password = hash;
+      next();
+    });
+  });
+});
+//COMPARE USER PASSWORD ENTERED TO HASHED PASSWORD
+UserSchema.methods.comparePassword = function (userPassword, cb) {
+  bcrypt.compare(userPassword, this.password, function (e, isMatch) {
+    if (e) return cb(e);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model("user", UserSchema);
